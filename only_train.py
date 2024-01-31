a217d6eabf0f7d018dae545ab73d5cc3511293ee
desrch <112937656+desrch@users.noreply.github.com>
2024-01-31 17:22:48 +0800

    Update only_train.py

--- only_train.py
+++ only_train.py
@@ -136,7 +136,7 @@ if __name__ == '__main__':
         transforms.CenterCrop(224),
         transforms.RandomHorizontalFlip(0.5),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.483, 0.452, 0.400], std=[0.224, 0.224, 0.224])
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 
     ])
 
